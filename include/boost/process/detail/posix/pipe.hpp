de0445751d887b42017263a82ae760c6f95be3bc
Klemens Morgenstern <klemens@debian>
2016-04-16 14:19:36 +0200

    Fixed include guard

--- include/boost/process/detail/posix/pipe.hpp
+++ include/boost/process/detail/posix/pipe.hpp
@@ -7,8 +7,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_PROCESS_WINDOWS_PIPE_HPP
-#define BOOST_PROCESS_WINDOWS_PIPE_HPP
+#ifndef BOOST_PROCESS_POSIX_PIPE_HPP
+#define BOOST_PROCESS_POSIX_PIPE_HPP
 
 
 #include <boost/filesystem.hpp>
