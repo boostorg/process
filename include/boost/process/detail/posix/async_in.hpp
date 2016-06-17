560c48d09f89a5707e3bc474aedb70329dbb4b5d
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-06-17 20:31:12 +0200

    doc update

--- include/boost/process/detail/posix/async_in.hpp
+++ include/boost/process/detail/posix/async_in.hpp
@@ -56,17 +56,17 @@ struct async_in_buffer : ::boost::process::detail::posix::async_handler
             boost::asio::async_write(*pipe, buf,
                 [pipe, promise](const boost::system::error_code & ec, std::size_t)
                 {
-            		if (ec && (ec.value() != EBADF) && (ec.value() == EPERM))
+                    if (ec && (ec.value() != EBADF) && (ec.value() == EPERM))
                     {
                         std::error_code e(ec.value(), std::system_category());
                         promise->set_exception(std::make_exception_ptr(std::system_error(e)));
                     }
-            		else
-            			promise->set_value();
+                    else
+                        promise->set_value();
                 });
         }
         else
-        	boost::asio::async_write(*pipe, buf,
+            boost::asio::async_write(*pipe, buf,
                 [pipe](const boost::system::error_code&ec, std::size_t size){});
 
         this->pipe = nullptr;
