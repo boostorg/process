8ffc6c5ec2e901371ab1fb49e197263e3034238a
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-11-06 00:24:52 +0100

    removed a warning for msvc

--- include/boost/process/detail/windows/wait_for_exit.hpp
+++ include/boost/process/detail/windows/wait_for_exit.hpp
@@ -67,7 +67,8 @@ inline bool wait_for(
     std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(rel_time);
 
     ::boost::detail::winapi::DWORD_ wait_code;
-    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(), ms.count());
+    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(),
+                   static_cast<::boost::detail::winapi::DWORD_>(ms.count()));
     if (wait_code == ::boost::detail::winapi::wait_failed)
         throw_last_error("WaitForSingleObject() failed");
     else if (wait_code == ::boost::detail::winapi::wait_timeout)
@@ -96,7 +97,8 @@ inline bool wait_for(
 
 
     ::boost::detail::winapi::DWORD_ wait_code;
-    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(), ms.count());
+    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(),
+    		         static_cast<::boost::detail::winapi::DWORD_>(ms.count()));
     if (wait_code == ::boost::detail::winapi::wait_failed)
         ec = std::error_code(
             ::boost::detail::winapi::GetLastError(),
@@ -133,7 +135,8 @@ inline bool wait_until(
                     timeout_time - std::chrono::system_clock::now());
 
     ::boost::detail::winapi::DWORD_ wait_code;
-    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(), ms.count());
+    wait_code = ::boost::detail::winapi::WaitForSingleObject(p.process_handle(),
+	                static_cast<::boost::detail::winapi::DWORD_>(ms.count()));
 
     if (wait_code == ::boost::detail::winapi::wait_failed)
         throw_last_error("WaitForSingleObject() failed");
@@ -165,7 +168,8 @@ inline bool wait_until(
     ::boost::detail::winapi::DWORD_ _exit_code = 1;
 
     if (::boost::detail::winapi::WaitForSingleObject(p.process_handle(),
-            ms.count()) == ::boost::detail::winapi::wait_failed)
+            static_cast<::boost::detail::winapi::DWORD_>(ms.count()))
+                == ::boost::detail::winapi::wait_failed)
         ec = std::error_code(
             ::boost::detail::winapi::GetLastError(),
             std::system_category());
