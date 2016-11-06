be4bd945e16ac48c4655beaa627c1a753c43f4c2
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-11-06 17:04:27 +0100

    removed conditional - extensions shall use handler not handler_base

--- include/boost/process/detail/posix/executor.hpp
+++ include/boost/process/detail/posix/executor.hpp
@@ -70,19 +70,9 @@ struct on_fork_error_t
     on_fork_error_t(Executor & exec, const std::error_code & error) : exec(exec), error(error) {};
 
     template<typename T>
-    void impl(T & t, std::true_type /* is extended */) const
-    {
-        t.on_fork_error(exec, error);
-    }
-
-    template<typename T>
-    void impl(T &t, std::false_type /* is extended */) const {}
-
-    template<typename T>
     void operator()(T & t) const
     {
-        using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
-        impl(t, std::is_base_of<T, type>());
+        t.on_fork_error(exec, error);
     }
 };
 
@@ -94,20 +84,9 @@ struct on_exec_setup_t
     on_exec_setup_t(Executor & exec) : exec(exec) {};
 
     template<typename T>
-    void impl(T & t, std::true_type /* is extended */) const
-    {
-        t.on_exec_setup(exec);
-    }
-
-    template<typename T>
-    void impl(T &t, std::false_type /* is extended */) const {}
-
-    template<typename T>
     void operator()(T & t) const
     {
-        using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
-        if (!exec.error())
-            impl(t, std::is_base_of<T, type>());
+        t.on_exec_setup(exec);
     }
 };
 
@@ -120,19 +99,9 @@ struct on_exec_error_t
     on_exec_error_t(Executor & exec, const std::error_code & error) : exec(exec), ec(error) {};
 
     template<typename T>
-    void impl(T & t, std::true_type /* is extended */) const
-    {
-        t.on_exec_error(exec, ec);
-    }
-
-    template<typename T>
-    void impl(T &t, std::false_type /* is extended */) const {}
-
-    template<typename T>
     void operator()(T & t) const
     {
-        using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
-        impl(t, std::is_base_of<T, type>());
+        t.on_exec_error(exec, ec);
     }
 };
 
