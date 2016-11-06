c5d3937e9ad03c29b3e72664f304821cb5184882
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-11-06 15:44:28 +0100

    added test for on_exit(future<int>())

--- test/async.cpp
+++ test/async.cpp
@@ -57,6 +57,31 @@ BOOST_AUTO_TEST_CASE(async_wait, *boost::unit_test::timeout(2))
 }
 
 
+BOOST_AUTO_TEST_CASE(async_future, *boost::unit_test::timeout(2))
+{
+    using boost::unit_test::framework::master_test_suite;
+    using namespace boost::asio;
+
+    boost::asio::io_service io_service;
+
+    int exit_code = 0;
+    std::error_code ec;
+    std::future<int> fut;
+    bp::child c(
+        master_test_suite().argv[1],
+        "test", "--exit-code", "42",
+        ec,
+        io_service,
+        bp::on_exit=fut
+    );
+
+    BOOST_REQUIRE(!ec);
+    io_service.run();
+    BOOST_REQUIRE(fut.valid());
+    BOOST_CHECK_EQUAL(fut.get(), 42);
+}
+
+
 BOOST_AUTO_TEST_CASE(async_out_stream, *boost::unit_test::timeout(2))
 {
     using boost::unit_test::framework::master_test_suite;
