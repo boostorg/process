449eef0b98c5b43a7a60e49f0867a6bab8fe7647
Klemens Morgenstern <klemens.morgenstern@mw-sc.de>
2017-08-02 09:37:06 +0200

    Update async.hpp

--- include/boost/process/async.hpp
+++ include/boost/process/async.hpp
@@ -118,7 +118,7 @@ chlid c2("ls", on_exit=exit_code);
 
 \note The handler is not invoked when the launch fails.
 \warning When used \ref ignore_error it might get invoked on error.
-\warning All `on_exit` use one signal(SIGCHLD) on poisx, which is only guaranteed to work when all use the same `io_service`.
+\warning All `on_exit` use one signal(SIGCHLD) on posix, which is only guaranteed to work when all use the same `io_service`.
  */
 constexpr static ::boost::process::detail::on_exit_ on_exit{};
 #endif
