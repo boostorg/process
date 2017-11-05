ab6e5b54ec864f6a6cd7fc64e752ab0636202f4a
Egor Pugin <egor.pugin@gmail.com>
2017-11-05 20:21:49 +0300

    Rename to io_context

--- test/async_pipe.cpp
+++ test/async_pipe.cpp
@@ -66,16 +66,16 @@ BOOST_AUTO_TEST_CASE(closed_transform)
 
 BOOST_AUTO_TEST_CASE(multithreaded_async_pipe)
 {
-    asio::io_service ios;
+    asio::io_context ioc;
 
     std::vector<std::thread> threads;
     for (int i = 0; i < std::thread::hardware_concurrency(); i++)
     {
-        threads.emplace_back([&ios]
+        threads.emplace_back([&ioc]
         {
             std::vector<bp::async_pipe*> pipes;
             for (size_t i = 0; i < 100; i++)
-                pipes.push_back(new bp::async_pipe(ios));
+                pipes.push_back(new bp::async_pipe(ioc));
             for (auto &p : pipes)
                 delete p;
         });
