a0ceebd59fcb6a9acdd06cb0e1f4432a1304beda
Alex Merry <alexander.merry@nanoporetech.com>
2018-02-22 09:02:02 +0000

    Fix copyright headers
    Insofar as there are copyrights on these changes, they are owned by my
    employer, as I'm doing this on company time.

--- test/args_handling.cpp
+++ test/args_handling.cpp
@@ -3,6 +3,7 @@
 // Copyright (c) 2009 Boris Schaeling
 // Copyright (c) 2010 Felipe Tanus, Boris Schaeling
 // Copyright (c) 2011, 2012 Jeff Flinn, Boris Schaeling
+// Copyright (c) 2018 Oxford Nanopore Technologies
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
