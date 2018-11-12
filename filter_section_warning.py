c2ee6da367c13432fe4d33ae9c3011ce40b5e99b
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2018-11-12 17:54:53 +0700

    trying to get the sanitization to work here

--- filter_section_warning.py
+++ filter_section_warning.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 #
-# filter-noisy-assembler-warnings.py
-# Author: Stuart Berg
 
 import sys
+import subprocess
 
-for line in sys.stdin:
+b2 = subprocess.Popen(sys.argv[1:], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+while not b2.returncode:
+    line = b2.stdout.readline()
+    if not line: break;
     # If line is a 'noisy' warning, don't print it or the following two lines.
     if ('warning: section' in line and 'is deprecated' in line
             or 'note: change section name to' in line):
         next(sys.stdin)
         next(sys.stdin)
     else:
-        sys.stderr.write(line)
-        sys.stderr.flush()
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
+exit(b2.returncode)
