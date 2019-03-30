b8821eac57d16d0bfa720da7d8688f66f81c94d5
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-03-30 23:14:25 +0800

    should close #78

--- include/boost/process/detail/windows/wait_group.hpp
+++ include/boost/process/detail/windows/wait_group.hpp
@@ -52,7 +52,7 @@ inline bool wait_impl(const group_handle & p, std::error_code & ec, std::chrono:
         {
             auto now = std::chrono::system_clock::now();
             auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time);
-            wait_time -= diff.count();
+            wait_time -= static_cast<std::chrono::system_clock::rep>(diff.count());
             start_time = now;
             if (wait_time <= 0)
                 return true; //timeout with other source
