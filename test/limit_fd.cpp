7fc41b2815822b632d524d5c17897abbdf0c4653
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-04-09 14:31:50 +0800

    added windows handles functionality

--- test/limit_fd.cpp
+++ test/limit_fd.cpp
@@ -1,8 +1,4 @@
-// Copyright (c) 2006, 2007 Julio M. Merino Vidal
-// Copyright (c) 2008 Ilya Sokolov, Boris Schaeling
-// Copyright (c) 2009 Boris Schaeling
-// Copyright (c) 2010 Felipe Tanus, Boris Schaeling
-// Copyright (c) 2011, 2012 Jeff Flinn, Boris Schaeling
+// Copyright (c) 2019 Klemens D. Morgenstern
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,158 +7,104 @@
 #define BOOST_TEST_IGNORE_SIGCHLD
 #include <boost/test/included/unit_test.hpp>
 
+#include <iostream>
+
 #include <boost/process.hpp>
-#include <boost/process/posix.hpp>
+#include <boost/process/handles.hpp>
+#include <boost/process/pipe.hpp>
+#include <boost/process/async_pipe.hpp>
 
 #include <boost/filesystem.hpp>
 
 #include <system_error>
-
-
 #include <string>
-#include <sys/wait.h>
-#include <errno.h>
+
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/udp.hpp>
+
+#if defined(BOOST_WINDOWS_API)
+#include <boost/winapi/get_current_thread.hpp>
+#include <boost/winapi/get_current_process.hpp>
+#endif
 
 namespace fs = boost::filesystem;
 namespace bp = boost::process;
-
-BOOST_AUTO_TEST_CASE(bind_fd, *boost::unit_test::timeout(2))
-{
-    using boost::unit_test::framework::master_test_suite;
-
-    bp::pipe p;
-
-    std::error_code ec;
-    bp::child c(
-        master_test_suite().argv[1],
-        "test", "--posix-echo-one", "3", "hello",
-        bp::posix::fd.bind(3, p.native_sink()),
-        ec
-    );
-    BOOST_CHECK(!ec);
-
-
-    bp::ipstream is(std::move(p));
-
-    std::string s;
-    is >> s;
-    BOOST_CHECK_EQUAL(s, "hello");
-}
-
-BOOST_AUTO_TEST_CASE(bind_fds, *boost::unit_test::timeout(2))
-{
-    using boost::unit_test::framework::master_test_suite;
-
-    bp::pipe p1;
-    bp::pipe p2;
-
-    std::error_code ec;
-    bp::child c(
-        master_test_suite().argv[1],
-        "test","--posix-echo-two","3","hello","99","bye",
-        bp::posix::fd.bind(3,  p1.native_sink()),
-        bp::posix::fd.bind(99, p2.native_sink()),
-        ec
-    );
-    BOOST_CHECK(!ec);
-
-    bp::ipstream is1(std::move(p1));
-    bp::ipstream is2(std::move(p2));
-
-    std::string s1;
-    is1 >> s1;
-    BOOST_CHECK_EQUAL(s1, "hello");
-
-    std::string s2;
-    is2 >> s2;
-    BOOST_CHECK_EQUAL(s2, "bye");
-}
-
-BOOST_AUTO_TEST_CASE(execve_set_on_error, *boost::unit_test::timeout(2))
-{
-    std::error_code ec;
-    bp::spawn(
-        "doesnt-exist",
-        ec
-    );
-    BOOST_CHECK(ec);
-    BOOST_CHECK_EQUAL(ec.value(), ENOENT);
-}
-
-BOOST_AUTO_TEST_CASE(execve_throw_on_error, *boost::unit_test::timeout(2))
-{
-    try
-    {
-        bp::spawn("doesnt-exist");
-        BOOST_CHECK(false);
-    }
-    catch (bp::process_error &e)
-    {
-        BOOST_CHECK(e.code());
-        BOOST_CHECK_EQUAL(e.code().value(), ENOENT);
-    }
-}
+namespace bt = boost::this_process;
 
 BOOST_AUTO_TEST_CASE(leak_test, *boost::unit_test::timeout(5))
 {
     using boost::unit_test::framework::master_test_suite;
 
+#if defined(BOOST_WINDOWS_API)
+    const auto get_handle       = [](FILE * f)                       {return reinterpret_cast<bt::native_handle_type>(_get_osfhandle(_fileno(f)));};
+    const auto socket_to_handle = [](::boost::winapi::UINT_PTR_ sock){return reinterpret_cast<::boost::winapi::HANDLE_>(sock);};
+#else
+    const auto get_handle = [](FILE * f) {return fileno(f);};
+    const auto socket_to_handle = [](int i){ return i;};
+#endif
+
     std::error_code ec;
+    auto fd_list = bt::get_handles(ec);
 
-    const auto pid = boost::this_process::get_id();
-    const auto  fd_path = fs::path("/proc") / std::to_string(pid) / "fd";
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), get_handle(stdin)),  1);
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), get_handle(stdout)), 1);
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), get_handle(stderr)), 1);
 
-    auto get_fds = [&]{
-        std::vector<int> fds;
-        for (auto && fd : fs::directory_iterator(fd_path))
-            fds.push_back(std::stoi(fd.path().filename().string()));
-        return fds;
-    };
-
-    std::vector<int> fd_list = get_fds();
-    if (fd_list.empty()) //then there's no /proc in the current linux distribution.
-        return;
+    BOOST_CHECK(bt::is_stream_handle(get_handle(stdin),  ec));      BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(get_handle(stdout), ec));      BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(get_handle(stderr), ec));      BOOST_CHECK_MESSAGE(!ec, ec.message());
 
 
-    BOOST_CHECK(std::find(fd_list.begin(), fd_list.end(), STDOUT_FILENO) != fd_list.end());
-    BOOST_CHECK(std::find(fd_list.begin(), fd_list.end(),  STDIN_FILENO) != fd_list.end());
-    BOOST_CHECK(std::find(fd_list.begin(), fd_list.end(), STDERR_FILENO) != fd_list.end());
+    BOOST_CHECK_GE(fd_list.size(), 3);
+    BOOST_CHECK_GE(bt::get_handles(ec).size(), fd_list.size());
 
-    bp::pipe p; //should add two descriptors.
+    bp::pipe p;
 
-    auto fd_list_new = get_fds();
+    {
 
-    BOOST_CHECK_EQUAL(fd_list_new.size(), fd_list.size() + 2);
-    fd_list.push_back(p.native_source());
-    fd_list.push_back(p.native_sink());
-
-
-    BOOST_CHECK_EQUAL(
-            bp::system(
-                    master_test_suite().argv[1],
-                    "test", "--exit-code", "123",  ec), 123);
-
-    fd_list_new = get_fds();
-    BOOST_CHECK_EQUAL(fd_list.size(), fd_list_new.size());
+        auto fd_list_new = bt::get_handles(ec);
+        BOOST_CHECK_MESSAGE(!ec, ec);
+        BOOST_CHECK_LE(fd_list.size() + 2, fd_list_new.size());
+        fd_list = std::move(fd_list_new);
+    }
 
 
-    const int native_source = p.native_source();
-    BOOST_CHECK_EQUAL(
-            bp::system(
-                    master_test_suite().argv[1],
-                    bp::std_in < p,
-                    "test", "--exit-code", "123",  ec), 123);
 
-    BOOST_CHECK(!ec);
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), p.native_source()), 1);
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), p.native_sink()),   1);
 
-   ////now, p.source should be closed, so we remove it from fd_list
+    BOOST_CHECK(bt::is_stream_handle(p.native_source(), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(p.native_sink(),   ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
 
-   const auto itr = std::find(fd_list.begin(), fd_list.end(), native_source);
-   if (itr != fd_list.end())
-       fd_list.erase(itr);
 
-    fd_list_new = get_fds();
+    p.close();
+    fd_list = bt::get_handles(ec);
 
-    BOOST_CHECK_EQUAL(fd_list.size(), fd_list_new.size());
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), p.native_source()), 0);
+    BOOST_CHECK_EQUAL(std::count(fd_list.begin(), fd_list.end(), p.native_sink()),   0);
 
+#if defined( BOOST_WINDOWS_API )
+    std::thread thr([]{});
+    BOOST_CHECK(!bt::is_stream_handle(thr.native_handle(), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
+    thr.join();
+#else
+
+#endif
+
+
+    boost::asio::io_context ioc;
+    boost::asio::ip::tcp::socket tcp_socket(ioc);
+    //tcp_socket.open();
+    boost::asio::ip::udp::socket udp_socket(ioc);
+    //udp_socket.open();
+
+    bp::async_pipe ap(ioc);
+
+    tcp_socket.open(boost::asio::ip::tcp::v4());
+    udp_socket.open(boost::asio::ip::udp::v4());
+
+    BOOST_CHECK(bt::is_stream_handle(socket_to_handle(tcp_socket.native_handle()), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(socket_to_handle(udp_socket.native_handle()), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(std::move(ap).sink().  native_handle(), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
+    BOOST_CHECK(bt::is_stream_handle(std::move(ap).source().native_handle(), ec)); BOOST_CHECK_MESSAGE(!ec, ec.message());
 }
