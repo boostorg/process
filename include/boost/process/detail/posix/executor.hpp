bbabea30dd0940ab604e332a50b38013138dd038
Klemens <klemens.d.morgenstern@gmail.com>
2022-08-19 00:49:57 +0800

    Added reaping child for execve error, closes #265.

--- include/boost/process/detail/posix/executor.hpp
+++ include/boost/process/detail/posix/executor.hpp
@@ -444,6 +444,8 @@ child executor<Sequence>::invoke(boost::mpl::false_, boost::mpl::false_)
     }
     if (_ec)
     {
+        //if an error occured we need to reap the child process
+        ::waitpid(this->pid, nullptr, WNOHANG);
         boost::fusion::for_each(seq, call_on_error(*this, _ec));
         return child();
     }
@@ -537,6 +539,7 @@ child executor<Sequence>::invoke(boost::mpl::false_, boost::mpl::true_)
 
     if (_ec)
     {
+        ::waitpid(this->pid, nullptr, WNOHANG);
         boost::fusion::for_each(seq, call_on_error(*this, _ec));
         return child();
     }
