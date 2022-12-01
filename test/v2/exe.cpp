fa9d9284bafc8f67ff4500cb5cc27b1f57ea304a
Samuel Venable <samuelvenable@hotmail.com>
2022-12-01 15:48:36 -0500

    Update exe.cpp

--- test/v2/exe.cpp
+++ test/v2/exe.cpp
@@ -3,13 +3,12 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/process/filesystem.hpp>
-
 #include <boost/process/v2/pid.hpp>
 #include <boost/process/v2/exe.hpp>
 
 #include <boost/test/unit_test.hpp>
 
+#include <string>
 #include <vector>
 
 BOOST_AUTO_TEST_CASE(test_exe)
@@ -17,11 +16,11 @@ BOOST_AUTO_TEST_CASE(test_exe)
     namespace bp2 = boost::process::v2;
 
     auto all = bp2::all_pids();
-    std::vector<filesystem::path> pths;
+    std::vector<std::string> pths;
     for (unsigned i = 0; i < all.size(); i++) {
-        auto pth = bp2::ext::exe_path(all[i]);
-        pths.push_back((!pth.string().empty()) ? pth.string() : "<unknown>");
+        auto pth = bp2::ext::exe_path(all[i]).string();
+        pths.push_back((!pth.empty()) ? pth : "<unknown>");
     }
     // Check failed if all executable paths are unknown.
-    BOOST_CHECK(std::count(pths.begin(), pths.end(), "<unknown>") != pths.size());
+    BOOST_CHECK_NE(std::count(pths.begin(), pths.end(), "<unknown>"), pths.size());
 }
