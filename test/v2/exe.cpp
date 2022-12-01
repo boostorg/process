3829b74a84c82a388715965fb4b2b19c21297de5
Samuel Venable <samuelvenable@hotmail.com>
2022-12-01 15:07:54 -0500

    Update exe.cpp

--- test/v2/exe.cpp
+++ test/v2/exe.cpp
@@ -3,6 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/process/filesystem.hpp>
+
 #include <boost/process/v2/pid.hpp>
 #include <boost/process/v2/exe.hpp>
 
