70c18200a790c0b0d40f7ff0fa146c32d1bb88b8
Samuel Venable <samuelvenable@hotmail.com>
2022-12-04 04:45:55 -0500

    [skip ci] update copyright

--- include/boost/process/v2/detail/exe.hpp
+++ include/boost/process/v2/detail/exe.hpp
@@ -1,13 +1,15 @@
 // Copyright (c) 2022 Klemens D. Morgenstern
+// Copyright (c) 2022 Samuel Venable
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_PROCESS_V2_DETAIL_EXE_HPP
 #define BOOST_PROCESS_V2_DETAIL_EXE_HPP
 
-#include <boost/process/v2/pid.hpp>
+#include <boost/process/v2/detail/config.hpp>
+#include <boost/process/v2/detail/throw_error.hpp>
 
-#include <system_error>
+#include <boost/process/v2/pid.hpp>
 
 #if defined(BOOST_PROCESS_V2_WINDOWS)
 #include <windows.h>
@@ -23,8 +25,8 @@ namespace ext
 
 #if defined(BOOST_PROCESS_V2_WINDOWS)
 
-BOOST_PROCESS_V2_DECL HANDLE open_process_with_debug_privilege(pid_type pid, error_code & ec);
-BOOST_PROCESS_V2_DECL BOOL is_x86_process(HANDLE proc, error_code & ec);
+BOOST_PROCESS_V2_DECL HANDLE open_process_with_debug_privilege(pid_type pid, boost::system::error_code & ec);
+BOOST_PROCESS_V2_DECL BOOL is_x86_process(HANDLE proc, boost::system::error_code & ec);
 
 #endif
 
