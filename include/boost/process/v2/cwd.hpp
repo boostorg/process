cd9a690c6b1a3924759b754e7e3c029f99247030
Samuel Venable <samuelvenable@hotmail.com>
2022-12-06 20:47:47 -0500

    Update cwd.hpp

--- include/boost/process/v2/cwd.hpp
+++ include/boost/process/v2/cwd.hpp
@@ -2,8 +2,8 @@
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#ifndef BOOST_PROCESS_V2_EXE_HPP
-#define BOOST_PROCESS_V2_EXE_HPP
+#ifndef BOOST_PROCESS_V2_CWD_HPP
+#define BOOST_PROCESS_V2_CWD_HPP
 
 #include <boost/process/v2/detail/config.hpp>
 #include <boost/process/v2/detail/throw_error.hpp>
@@ -27,5 +27,4 @@ BOOST_PROCESS_V2_DECL boost::filesystem::path current_path(boost::process::v2::p
 
 BOOST_PROCESS_V2_END_NAMESPACE
 
-#endif // BOOST_PROCESS_V2_EXE_HPP
-
+#endif // BOOST_PROCESS_V2_CWD_HPP
