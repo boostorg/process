d6bcb0014ab227cddcf8e9a51e257ed38bbe38f4
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-02-06 20:33:50 +0800

    switched to BOOST_DEPRECATED.

--- include/boost/process/group.hpp
+++ include/boost/process/group.hpp
@@ -126,7 +126,7 @@ public:
     /** Wait for the process group to exit for period of time.
       *  \return True if all child processes exited while waiting.*/
     template< class Rep, class Period >
-    BOOST_PROCESS_DEPRECATED("wait_for is unreliable")
+    BOOST_DEPRECATED("wait_for is unreliable")
     bool wait_for  (const std::chrono::duration<Rep, Period>& rel_time)
     {
         return boost::process::detail::api::wait_for(_group_handle, rel_time);
@@ -134,7 +134,7 @@ public:
 
     /** \overload bool wait_for(const std::chrono::duration<Rep, Period>& timeout_time ) */
     template< class Rep, class Period >
-    BOOST_PROCESS_DEPRECATED("wait_for is unreliable")
+    BOOST_DEPRECATED("wait_for is unreliable")
     bool wait_for  (const std::chrono::duration<Rep, Period>& rel_time, std::error_code & ec) noexcept
     {
         return boost::process::detail::api::wait_for(_group_handle, rel_time, ec);
@@ -143,14 +143,14 @@ public:
     /** Wait for the process group to exit until a point in time.
       *  \return True if all child processes exited while waiting.*/
     template< class Clock, class Duration >
-    BOOST_PROCESS_DEPRECATED("wait_until is unreliable")
+    BOOST_DEPRECATED("wait_until is unreliable")
     bool wait_until(const std::chrono::time_point<Clock, Duration>& timeout_time )
     {
         return boost::process::detail::api::wait_until(_group_handle, timeout_time);
     }
     /** \overload bool wait_until(const std::chrono::time_point<Clock, Duration>& timeout_time ) */
     template< class Clock, class Duration >
-    BOOST_PROCESS_DEPRECATED("wait_until is unreliable")
+    BOOST_DEPRECATED("wait_until is unreliable")
     bool wait_until(const std::chrono::time_point<Clock, Duration>& timeout_time, std::error_code & ec) noexcept
     {
         return boost::process::detail::api::wait_until(_group_handle, timeout_time, ec);
