boostorg/process

commit 1a1d677d769f4016422c0b26de971f43ec91a20f

Closes #202

include/boost/process/detail/posix/executor.hpp
@@ -151,6 +151,7 @@ class executor
 
     int _pipe_sink = -1;
 
+
     void write_error(const std::error_code & ec, const char * msg)
     {
         //I am the child
@@ -327,6 +328,13 @@ public:
     }
     void set_error(const std::error_code &ec, const std::string &msg) {set_error(ec, msg.c_str());};
 
+    std::vector<int> get_used_handles() const 
+    {
+        if (_pipe_sink == -1)
+            return {};
+        else
+            return {_pipe_sink};
+    };
 };
 
 template<typename Sequence>

include/boost/process/detail/used_handles.hpp
@@ -69,7 +69,7 @@ template<typename Executor>
 std::vector<::boost::process::detail::api::native_handle_type>
         get_used_handles(Executor &exec)
 {
-    std::vector<::boost::process::detail::api::native_handle_type> res;
+    std::vector<::boost::process::detail::api::native_handle_type> res = exec.get_used_handles();
     foreach_used_handle(exec, [&](::boost::process::detail::api::native_handle_type handle){res.push_back(handle);});
     return res;
 }

commit 88435eac1e629efcea8efaf45519c30a617da783

posix: Fix exec error reporting with limit_handles
_pipe_sink was assigned after call_on_setup(), after limit_fd_::on_setup(),
but this was too late. It must be assigned earlier so that
executor::get_used_handles() can see it and prevent limit_handles from
closing the internal pipe for passing exec() errors from child to parent.

Fixes: 1a1d677d
Closes: https://github.com/boostorg/process/issues/202
Signed-off-by: Daniel Klauer <daniel.klauer@gin.de>

include/boost/process/detail/posix/executor.hpp
@@ -398,12 +398,16 @@ child executor<Sequence>::invoke(boost::mpl::false_, boost::mpl::false_)
             set_error(err, "fcntl(2) failed");//this might throw, so we need to be sure our pipe is safe.
             return child();
         }
+
+        _pipe_sink = p.p[1];
+
         _ec.clear();
         boost::fusion::for_each(seq, call_on_setup(*this));
 
         if (_ec)
         {
             boost::fusion::for_each(seq, call_on_error(*this, _ec));
+            _pipe_sink = -1;
             return child();
         }
 
@@ -417,11 +421,11 @@ child executor<Sequence>::invoke(boost::mpl::false_, boost::mpl::false_)
             _msg = "fork() failed";
             boost::fusion::for_each(seq, call_on_fork_error(*this, _ec));
             boost::fusion::for_each(seq, call_on_error(*this, _ec));
+            _pipe_sink = -1;
             return child();
         }
         else if (pid == 0)
         {
-            _pipe_sink = p.p[1];
             ::close(p.p[0]);
 
             boost::fusion::for_each(seq, call_on_exec_setup(*this));
@@ -439,6 +443,7 @@ child executor<Sequence>::invoke(boost::mpl::false_, boost::mpl::false_)
 
         ::close(p.p[1]);
         p.p[1] = -1;
+        _pipe_sink = -1;
         _read_error(p.p[0]);
 
     }

test/limit_fd.cpp
@@ -193,3 +193,9 @@ BOOST_AUTO_TEST_CASE(limit_fd, *boost::unit_test::timeout(5))
 
     fclose(p);
 }
+
+BOOST_AUTO_TEST_CASE(limit_fd_does_not_break_error_reporting, *boost::unit_test::timeout(5))
+{
+    BOOST_CHECK_THROW(boost::process::system("/does/not/exist"), boost::process::process_error);
+    BOOST_CHECK_THROW(boost::process::system("/does/not/exist", boost::process::limit_handles), boost::process::process_error);
+}